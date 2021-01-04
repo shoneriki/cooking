e335b8e108a036e3b6c44b191284205d45b82417
shoneriki <ikirenohs@gmail.com>
2021-01-04 16:41:25 +0900

    seeds for ingredient

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts "Clearing out Dish list and portions list"
+Dish.destroy_all
+puts "Clearing out Ingredients now"
+Ingredient.destroy_all
+
+url = "https://www.themealdb.com/api/json/v1/1/list.php?i=list"
+json = open(url).read
+result = JSON.parse(json)
+
+puts "Creating ingredients?"
+
+result["meals"].each do |meal|
+  Ingredient.create(name: meal["strIngredient"])
+end
+
+puts "Great! Finished creating #{Ingredient.count} ingredients!"
