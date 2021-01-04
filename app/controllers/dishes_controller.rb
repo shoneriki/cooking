2da5f87f79fb6eeaaee8dac4a434f2eb1e00aef8
shoneriki <ikirenohs@gmail.com>
2021-01-04 21:23:54 +0900

    create dish

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -7,8 +7,23 @@ class DishesController < ApplicationController
     @dish = Dish.new
   end
 
+  def create
+    @dish = Dish.new(dish_params)
+    if @dish.save
+      redirect_to dish_path(@dish)
+    else
+      render :new
+    end
+  end
+
   def show
     @dish = Dish.find(params[:id])
+  end
+
+  private
+
+  def dish_params
+    params.require(:dish).permit(:name)
   end
 
 end
