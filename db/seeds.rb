286dffaa02f2f2987c454306834daec4aceca543
shoneriki <ikirenohs@gmail.com>
2021-01-05 15:10:52 +0900

    grilled salmon recipe added

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,63 @@ result["meals"].each do |meal|
 end
 
 puts "Great! Finished creating #{Ingredient.count} ingredients!"
+
+puts "Creating Grilled Salmon recipe"
+
+@gs = Dish.create!(
+  name: 'Grilled Salmon'
+)
+file = URI.open("https://www.foodiecrush.com/wp-content/uploads/2019/05/Grilled-Salmon-foodiecrush.com-023.jpg")
+@gs.photo.attach(io: file, filename: 'gs.jpg', content_type: 'image/jpg')
+
+Portion.create!(
+  description: "1 1/2 pound",
+  dish: @gs,
+  ingredient: Ingredient.find_by(name: "Salmon")
+)
+
+Portion.create!(
+  description: "Preferred Serving Of",
+  dish: @gs,
+  ingredient: Ingredient.new(name: "Lemon Pepper")
+)
+
+Portion.create!(
+  description: "Preferred Serving Of",
+  dish: @gs,
+  ingredient: Ingredient.find_by(name: "Garlic Powder")
+)
+
+Portion.create!(
+  description: "Preferred Serving Of",
+  dish: @gs,
+  ingredient: Ingredient.find_by(name: "Salt")
+)
+
+Portion.create!(
+  description: "1/3 Cup",
+  dish: @gs,
+  ingredient: Ingredient.find_by(name: "Soy Sauce")
+)
+
+Portion.create!(
+  description: "1/3 Cup",
+  dish: @gs,
+  ingredient: Ingredient.find_by(name: "Brown Sugar")
+)
+
+Portion.create!(
+  description: "1/3 Cup",
+  dish: @gs,
+  ingredient: Ingredient.find_by(name: "Water")
+)
+
+Portion.create!(
+  description: "1/4 Cup",
+  dish: @gs,
+  ingredient: Ingredient.find_by(name: "Vegetable Oil")
+)
+
+puts "Finished Grilled Salmon Recipe"
+
+puts "Great! Finished creating #{Dish.count} recipes and #{Ingredient.count} ingredients"
